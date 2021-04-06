73941f20743c38533d405301db59af8404a06ab6
Pavel Kolchkov <kolchkovpavel@gmail.com>
2021-04-06 16:02:25 +0300

    add eslint and bugs fixed

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
-//export const name = readlineSync.question();
-export const greeting = (greet) => {
-    const name = readlineSync.question(greet);
-    console.log('Hello, ' + name + '!');
+
+const greeting = (greet) => {
+  const name = readlineSync.question(greet);
+  console.log(`Hello,${name}!`);
 };
+export default greeting;
